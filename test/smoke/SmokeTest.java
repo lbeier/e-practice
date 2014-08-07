e9974bccc472a736229078168c0777758419b958
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-07 00:35:53 -0300

    REFATORACAO: Lucas Beier - Teste agora utiliza BaseTestServer.

--- test/smoke/SmokeTest.java
+++ test/smoke/SmokeTest.java
@@ -1,23 +1,16 @@
 package smoke;
 
-import org.junit.Ignore;
+import TestHelpers.BaseTestServer;
+
 import org.junit.Test;
-import play.libs.F;
-import play.test.TestBrowser;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static play.test.Helpers.*;
 
-public class SmokeTest {
+public class SmokeTest extends BaseTestServer {
 
     @Test
     public void verificaSeOTituloEIgualAePractice() {
-        running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo(System.getenv("URL_ENVIRONMENT"));
-                assertThat(browser.$("title").getTexts().get(0)).isEqualTo("e-Practice");
-            }
-        });
+        testBrowser.goTo(URL_ENVIRONMENT);
+        assertThat(testBrowser.$("title").getTexts().get(0)).isEqualTo("e-Practice");
     }
-
 }
