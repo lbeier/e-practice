1649d5f02dbedf0a9f4429db013d762114591b1e
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-07 00:36:08 -0300

    REFATORACAO: Lucas Beier - Teste agora utiliza BaseTestServer.

--- test/unitario/controller/ApplicationTest.java
+++ test/unitario/controller/ApplicationTest.java
@@ -1,49 +1,35 @@
 package unitario.controller;
 
+import TestHelpers.BaseTestServer;
 import org.junit.Test;
-import play.libs.F;
 import play.mvc.Result;
-import play.test.TestBrowser;
-
 import java.util.HashMap;
-
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.withName;
 import static play.test.Helpers.*;
 
-public class ApplicationTest {
+public class ApplicationTest extends BaseTestServer {
+
     @Test
     public void quandoChamaOMetodoIndexRedirecionaParaOutraRota() {
         Result result;
-
         result = callAction(controllers.routes.ref.Application.index());
-
         assertThat(status(result)).isEqualTo(SEE_OTHER);
     }
 
     @Test
     public void quandoChamaOMetodoCriaExerciciosRedirecionaParaOutraRota() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                Result result;
-
-                result = callAction(controllers.routes.ref.Application.criaExercicios());
-
-                assertThat(status(result)).isEqualTo(SEE_OTHER);
-            }
-        });
+        Result result;
+        result = callAction(controllers.routes.ref.Application.criaExercicios());
+        assertThat(status(result)).isEqualTo(SEE_OTHER);
     }
+
     @Test
     public void rotaSolucoesComMetodoPostDeveRenderizarUmaView() throws Exception {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                final HashMap<String, String> fakeForm = new HashMap<String, String>();
-                fakeForm.put("solucaoDoUsuario", "var x: Integer");
-
-                Result result = routeAndCall(fakeRequest(POST, "/solucoes").withFormUrlEncodedBody(fakeForm));
-                assertThat(result).isNotNull();
-            }
-        });
+        final HashMap<String, String> fakeForm = new HashMap<String, String>();
+        fakeForm.put("solucaoDoUsuario", "var x: Integer");
+        Result result = routeAndCall(fakeRequest(POST, "/solucoes").withFormUrlEncodedBody(fakeForm));
+        assertThat(result).isNotNull();
     }
 
     @Test
@@ -54,51 +40,33 @@ public class ApplicationTest {
 
     @Test
     public void quandoPostaNovaSolucaoRetornaMensagemDeSucesso() throws Exception {
+        testBrowser.goTo(URL_ENVIRONMENT);
+        testBrowser.fill("#solucaoDoUsuario").with("var x  = 1");
+        testBrowser.$("#botaoDeEnviar").click();
+        testBrowser.$("#solucaoDoUsuario").submit();
 
-        running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-
-                browser.goTo(System.getenv("URL_ENVIRONMENT"));
-                browser.fill("#solucaoDoUsuario").with("var x  = 1");
-                browser.$("#botaoDeEnviar").click();
-                browser.$("#solucaoDoUsuario").submit();
-
-                assertThat(browser.url()).isEqualTo(System.getenv("URL_ENVIRONMENT")+"/solucoes");
-                assertThat(browser.$("#status", 0).getText()).isEqualTo("Status: sua solução foi salva com sucesso!");
-            }
-        });
+        assertThat(testBrowser.getBaseUrl() + testBrowser.url()).isEqualTo(URL_ENVIRONMENT + "/solucoes");
+        assertThat(testBrowser.$("#status", 0).getText()).isEqualTo("Status: sua solução foi salva com sucesso!");
     }
 
     @Test
     public void mantemASolucaoNaCaixaDeTextoAposEnviarASolucao() throws Exception {
-        running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-            @Override
-            public void invoke(TestBrowser browser) throws Throwable {
-                browser.goTo(System.getenv("URL_ENVIRONMENT"));
-                browser.fill("#solucaoDoUsuario").with("var esseTextoSeraMantido : String");
+        testBrowser.goTo(URL_ENVIRONMENT);
+        testBrowser.fill("#solucaoDoUsuario").with("var esseTextoSeraMantido : String");
+        testBrowser.find("button", withName("valor")).click();
 
-                browser.find("button", withName("valor")).click();
-
-                String textoDaTextArea = browser.$("#solucaoDoUsuario").getText();
-                assertThat(textoDaTextArea).contains("var esseTextoSeraMantido : String");
-            }
-        });
+        String textoDaTextArea = testBrowser.$("#solucaoDoUsuario").getText();
+        assertThat(textoDaTextArea).contains("var esseTextoSeraMantido : String");
     }
 
     @Test
     public void quandoPostaSolucaoEmBrancoRetornaMensagemDeErro() throws Exception {
-        running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
+        testBrowser.goTo(URL_ENVIRONMENT);
+        testBrowser.fill("#solucaoDoUsuario").with("");
+        testBrowser.$("#botaoDeEnviar").click();
+        testBrowser.$("#solucaoDoUsuario").submit();
+        String label = testBrowser.$("#status").getText();
 
-                browser.goTo(System.getenv("URL_ENVIRONMENT"));
-                browser.fill("#solucaoDoUsuario").with("");
-                browser.$("#botaoDeEnviar").click();
-                browser.$("#solucaoDoUsuario").submit();
-                String label = browser.$("#status").getText();
-
-                assertThat(label).isEqualTo("Status: erro!");
-            }
-        });
+        assertThat(label).isEqualTo("Status: erro!");
     }
-
 }
