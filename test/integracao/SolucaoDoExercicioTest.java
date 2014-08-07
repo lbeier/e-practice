3035177c587c853d41b098630ca12b14112fa406
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-07 00:35:17 -0300

    REFATORACAO: Lucas Beier - Teste agora utiliza BaseFakeApplication.

--- test/integracao/SolucaoDoExercicioTest.java
+++ test/integracao/SolucaoDoExercicioTest.java
@@ -1,28 +1,22 @@
 package integracao;
 
+import TestHelpers.BaseFakeApplication;
 import models.SolucaoDoExercicio;
 import org.junit.Ignore;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
-import static play.test.Helpers.fakeApplication;
-import static play.test.Helpers.running;
 
 
-public class SolucaoDoExercicioTest {
+public class SolucaoDoExercicioTest extends BaseFakeApplication {
     @Ignore
     @Test
     public void quandoChamaOMetodoCreateSalvaUmaSolucaoNoBanco() {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-            SolucaoDoExercicio novaSolucao = new SolucaoDoExercicio("var x = 1");
+        SolucaoDoExercicio novaSolucao = new SolucaoDoExercicio("var x = 1");
 
-            novaSolucao.save();
-            SolucaoDoExercicio novaSolucaoNoBanco = SolucaoDoExercicio.find.ref(novaSolucao.id);
+        novaSolucao.save();
+        SolucaoDoExercicio novaSolucaoNoBanco = SolucaoDoExercicio.find.ref(novaSolucao.id);
 
-            assertEquals(novaSolucaoNoBanco.solucaoDoUsuario, "var x = 1");
-            SolucaoDoExercicio.delete(novaSolucao.id);
-            }
-        });
+        assertEquals(novaSolucaoNoBanco.solucaoDoUsuario, "var x = 1");
+        SolucaoDoExercicio.delete(novaSolucao.id);
     }
 }
