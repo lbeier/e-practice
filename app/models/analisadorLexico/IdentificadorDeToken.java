8320d9d1dc4cd17d2d4c5489c0cc89e19708dcbb
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-07 00:34:41 -0300

    REFATORACAO: Lucas Beier - Refatoração para utilizar HashMap ao invés de if-else-if. Criados três métodos que utilizam expressões regulares para validar tokens.

--- app/models/analisadorLexico/IdentificadorDeToken.java
+++ app/models/analisadorLexico/IdentificadorDeToken.java
@@ -1,49 +1,68 @@
 package models.analisadorLexico;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IdentificadorDeToken {
 
+    private static final Map<String, String> SIMBOLOS_RESERVADOS = new HashMap<String, String>() {{
+        put("var", "PALAVRA_RESERVADA");
+        put("String", "TIPO_DE_VARIAVEL");
+        put("varres", "PALAVRA_RESERVADA");
+        put("Inteiro", "TIPO_DE_VARIAVEL");
+        put("=", "IGUAL");
+        put(":", "DECLARACAO");
+        put("+", "ADICAO");
+        put("-", "SUBTRACAO");
+        put("*", "MULTIPLICACAO");
+        put("/", "DIVISAO");
+        put("(", "PARENTESES_ABERTO");
+        put(")", "PARENTESES_FECHADO");
+        put("<>", "CONCATENACAO");
+    }};
+
     public String identifica(String token) {
-        if (token.equals("var")) {
-            return "PALAVRA_RESERVADA";
-        } else if (token.equals("String")) {
-            return "TIPO_DE_VARIAVEL";
-        } else if (token.equals("varres")) {
-            return "PALAVRA_RESERVADA";
-        } else if (token.equals("Inteiro")) {
-            return "TIPO_DE_VARIAVEL";
-        } else if (token.equals("=")) {
-            return "IGUAL";
-        } else if (token.equals(":")) {
-            return "DECLARACAO";
-        } else if (token.equals("+")) {
-            return "ADICAO";
-        } else if (token.equals("-")) {
-            return "SUBTRACAO";
-        } else if (token.equals("*")) {
-            return "MULTIPLICACAO";
-        } else if (token.equals("/")) {
-            return "DIVISAO";
-        } else if (token.equals("(")) {
-            return "PARENTESES_ABERTO";
-        } else if (token.equals(")")) {
-            return "PARENTESES_FECHADO";
-        } else if (token.equals("<>")) {
-            return "CONCATENACAO";
-        } else if (token.charAt(0) == '\"' && token.charAt(token.length()-1) == '\"') {
-            return "CONSTANTE_TIPO_STRING";
-        } else if (Character.isLetter(token.charAt(0))) {
+
+        if(SIMBOLOS_RESERVADOS.containsKey(token)) {
+            return SIMBOLOS_RESERVADOS.get(token);
+        }
+
+        // TODO: O que IDV significa?
+        if (tokenComecaComLetra(token)) {
             return "IDV";
-        } else if (Character.isDigit(token.charAt(0))) {
-            if(!verificaSeTodasOsCaracteresSaoNumeros(token)) return "ERRO";
-            else return "NUMERO";
-        } else return "INVALIDO";
+        }
+
+        if (tokenComecaETerminaComAspas(token)) {
+            return "CONSTANTE_TIPO_STRING";
+        }
+
+        if (verificaSeTodasOsCaracteresSaoNumeros(token)) {
+            return "NUMERO";
+        }
+
+        return "ERRO";
+    }
+
+    private static boolean tokenComecaComLetra(String token) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z_]");
+        Matcher matcher = pattern.matcher(token);
+
+        return matcher.find();
+    }
+
+    private static boolean tokenComecaETerminaComAspas(String token) {
+        Pattern pattern = Pattern.compile("\".*\"");
+        Matcher matcher = pattern.matcher(token);
+
+        return matcher.find();
     }
 
     public boolean verificaSeTodasOsCaracteresSaoNumeros(String token){
-        for (int i = 1; i < token.length(); i++) {
-            if(!Character.isDigit(token.charAt(i))){
-                return false;
-            }
-        }
-        return true;
+        Pattern pattern = Pattern.compile("^[0-9]+$");
+        Matcher matcher = pattern.matcher(token);
+
+        return matcher.find();
     }
 }
