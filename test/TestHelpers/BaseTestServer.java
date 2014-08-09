68f34808ab5d0829fe2dae8df77bc8cbad2bc78f
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-08 23:18:06 -0300

    REFATORACAO: Refatorado arquivo application para usar a variável de ambiente USER e o BaseTestServer para usar a baseUrl definida nas propriedades.

--- test/TestHelpers/BaseTestServer.java
+++ test/TestHelpers/BaseTestServer.java
@@ -3,17 +3,17 @@ package TestHelpers;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
+import play.Play;
 import play.test.Helpers;
 import play.test.TestBrowser;
 import play.test.TestServer;
-
 import static play.test.Helpers.HTMLUNIT;
 
 public abstract class BaseTestServer extends BaseFakeApplication {
 
     protected static TestBrowser testBrowser;
     protected static TestServer testServer;
-    protected static final String URL_ENVIRONMENT = System.getenv("URL_ENVIRONMENT");
+    protected static final String URL_ENVIRONMENT = Play.application().configuration().getString("application.baseUrl");
 
     @BeforeClass
     public static void setup() throws Exception {
