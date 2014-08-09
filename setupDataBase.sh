60b93faefa362b1583dd57ca1a2077992c4c7aba
Lucas Falk Beier <lucas.beier@gmail.com>
2014-08-09 01:49:06 -0300

    REFATORACAO: Alterador arquivo de criação do BD e de propriedades.

---
+++ setupDataBase.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+red=$(tput setaf 1)
+blue=$(tput setaf 4)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
+
+echo "Criando banco de dados com a seguinte configuracao:"
+echo "Banco de dados: ${blue}e_practice${reset}"
+echo "Usuario: ${blue}aceleradora${reset}"
+echo "Senha: ${blue}aceleradora${reset}"
+
+echo "${yellow}Insira a senha do Postgres:${reset}"
+
+if cat ./db.sql | psql
+then
+  echo "${green}[OK] O banco de dados foi configurado com sucesso!${reset}"
+else
+  echo "${red}[ERRO] Ocorreu um erro durante a configuração do banco de dados.${reset}"
+fi
